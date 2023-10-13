e4fad8c2cdd4c52e588782835c14572e717a8cf1
Sandip Nepali <sandeep47nepali@gmail.com>
2023-10-13 16:36:03 +0545

    add cube game

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,9 +1,4 @@
-import Project1 from "./assets/project1.jpeg";
-import Coming from "./assets/cm.jpg";
 import Coin from "./assets/projects/coin.gif";
-
-import TechPalika from "./assets/figma-design/Techpalika-2.jpg";
-// import Project6 from "./assets/project6.jpg"
 import html from "./assets/skills/html1.png";
 import css from "./assets/skills/css1.png";
 import javascript from "./assets/skills/js.png";
@@ -17,6 +12,9 @@ import github from "./assets/skills/github.png";
 import Todo from "./assets/projects/Todo.png";
 import Tictactoe from "./assets/projects/tic-tac-toe.png";
 import Form from "./assets/projects/form.png";
+import Tour from "./assets/projects/tour.png";
+import Cuberun from "./assets/projects/cube.gif";
+
 export const links = [
   {
     name: "Home",
@@ -68,38 +66,24 @@ export const projects = [
     link2: "https://github.com/sandeepNpl/Form-Collection",
     demo: "Demo",
   },
-
-  // {
-  //   id: 4,
-  //   img: Coming,
-  //   category: "Logo",
-  //   title: "Rocket",
-  //   link: "https://www.youtube.com/",
-  //   demo: "Steps",
-  // },
-  // {
-  //   id: 5,
-  //   img: Coming,
-  //   category: "Development",
-  //   title: "Stay Fit",
-  //   link: "https://www.youtube.com/",
-  //   demo: "Demo",
-  // },
-  // {
-  //   id: 6,
-  //   img: Coming,
-  //   category: "Development",
-  //   title: "Kana",
-  //   link: "https://www.youtube.com/",
-  //   demo: "Demo",
-  // },
-  // {
-  //   id: 6,
-  //   img: TechPalika,
-  //   category: "Figma",
-  //   title: "Techpalika",
-  //   link: "https://www.figma.com/community/file/1261653827657687725",
-  // },
+  {
+    id: 4,
+    img: Tour,
+    category: "Easy",
+    title: "Nepal Tour",
+    link1: "https://nepal-tour.vercel.app/",
+    link2: "https://github.com/sandeepNpl/Nepal-Tour#nepal-tour",
+    demo: "Demo",
+  },
+  {
+    id: 4,
+    img: Cuberun,
+    category: "Hard",
+    title: "Cube Run Game",
+    link1: "https://npbtsandip.itch.io/cuberun-web-game/",
+    link2: "https://github.com/sandeepNpl/CUBERUN-Game",
+    demo: "Demo",
+  },
 ];
 
 export const skills = [
